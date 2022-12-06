76de59bce3dae8cbec5608ed0b2a59cbf55710b3
frontenddevtrainer <praveen.trainer@outlook.com>
2022-12-06 09:54:49 +0000

    Bootstrap: Install

--- src/pages/post-detail.jsx
+++ src/pages/post-detail.jsx
@@ -2,29 +2,59 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { api } from "../config/urls"
 import { Link, Outlet } from "react-router-dom"
+import Accordion from 'react-bootstrap/Accordion';
 
-export default function PostDetailpage(){
+export default function PostDetailpage() {
 
     const { postid, userid } = useParams();
     const [post, setPost] = useState({});
 
-    async function getPostData(postid){
+    async function getPostData(postid) {
         // api.posts + "/" + postid
         const response = await (await fetch(`${api.posts}/${postid}`)).json();
         setPost(response);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getPostData(postid);
     }, [postid, userid])
 
     return <div>
         <h2>{post.title}</h2>
         {post.body}
+
+
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>Accordion Item #2</Accordion.Header>
+                <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+
         <div>
             <Link to={`/posts/${post.id}/${post.userId}/comments`}>Comments</Link>
             <Link to={`/posts/${post.id}/${post.userId}/offers`}>Offers</Link>
         </div>
-        <Outlet/>
+        <Outlet />
     </div>
 }
